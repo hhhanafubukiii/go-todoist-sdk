1a154bbe171877d069fa26b3f6d6a18e0675dd99
Hana <artemlugovoy201006@gmail.com>
2024-11-30 23:41:20 +0300

    Add files via upload

---
+++ authorization.go
@@ -0,0 +1,112 @@
+package go_todoist_sdk
+
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"io"
+	"log"
+	"net/http"
+)
+
+func (t *Client) GetAuthenticationURL(clientId, scope, state string) (string, error) {
+	if scope == "" || clientId == "" || state == "" {
+		panic("Empty values!")
+	}
+
+	authRequest := &AuthorizationRequest{
+		clientId: clientId,
+		scope:    scope,
+		state:    state,
+	}
+
+	authorizationURL, err := t.getUrl(AUTHORIZE_ENDPOINT)
+	if err != nil {
+		panic(err)
+	}
+
+	return fmt.Sprintf("%s?client_id=%s&scope=%s&state=%s", authorizationURL,
+		authRequest.clientId,
+		authRequest.scope,
+		authRequest.state), nil
+}
+
+func (t *Client) GetAccessToken(clientId, clientSecret, code string) (string, error) {
+	if clientSecret == "" || clientId == "" || code == "" {
+		panic("Empty values!")
+	}
+
+	tokenRequest := &TokenRequest{
+		clientId:     clientId,
+		clientSecret: clientSecret,
+		code:         code,
+	}
+
+	requestBody, err := tokenRequest.setBody()
+	if err != nil {
+		return "", err
+	}
+
+	accessToken, err := t.doHTTP(
+		TOKEN_ENDPOINT,
+		code,
+		tokenRequest.clientId,
+		tokenRequest.clientSecret,
+		requestBody,
+	)
+	if err != nil {
+		return accessToken, err
+	}
+
+	return accessToken, nil
+}
+
+func (t *Client) doHTTP(path, code, clientID, clientSecret string, reqBody []byte) (string, error) {
+	client := t.client
+	tokenResponse := TokenResponse{}
+
+	bodyReader := bytes.NewReader(reqBody)
+	uri, _ := t.getUrl(path)
+	requestURL := fmt.Sprintf("%s?client_id=%s&client_secret=%s&code=%s", uri, clientID, clientSecret, code)
+
+	req, err := http.NewRequest(http.MethodPost, requestURL, bodyReader)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	response, err := client.Do(req)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	body, err := io.ReadAll(response.Body)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	err = json.Unmarshal(body, &tokenResponse)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	return tokenResponse.Token, nil
+}
+
+func (t *Client) getUrl(path string) (string, error) {
+	return fmt.Sprintf("%s/%s", AUTH_BASE_URL, path), nil
+}
+
+func (t *TokenRequest) setBody() ([]byte, error) {
+	s := fmt.Sprintf(`{
+							"client_id": %s
+							"client_secret": %s
+							"code": %s
+							}`, t.clientId, t.clientSecret, t.code)
+
+	body, err := json.Marshal(s)
+	if err != nil {
+		return []byte{}, err
+	}
+
+	return body, nil
+}
---
+++ endpoints.go
@@ -0,0 +1,23 @@
+package go_todoist_sdk
+
+const (
+	BASE_URL      = "https://api.todoist.com"
+	AUTH_BASE_URL = "https://todoist.com"
+	SYNC_VERSION  = "v9"
+	REST_VERSION  = "v2"
+
+	TASKS_ENDPOINT         = "tasks"
+	PROJECTS_ENDPOINT      = "projects"
+	COLLABORATORS_ENDPOINT = "collaborators"
+	SECTIONS_ENDPOINT      = "sections"
+	COMMENTS_ENDPOINT      = "comments"
+	LABELS_ENDPOINT        = "labels"
+	SHARED_LABELS_ENDPOINT = "labels/shared"
+	QUICK_ADD_ENDPOINT     = "quick/add"
+
+	AUTHORIZE_ENDPOINT    = "oauth/authorize"
+	TOKEN_ENDPOINT        = "oauth/access_token"
+	REVOKE_TOKEN_ENDPOINT = "access_tokens/revoke"
+
+	COMPLETED_ITEMS_ENDPOINT = "archive/items"
+)
